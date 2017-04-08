6812051b01b798c7cab72f5665a2f5a762e01f3f
Alex Yu <alexyu@umd.edu>
2017-04-08 14:47:35 -0400

    send id to controller

--- flair-flask.py
+++ flair-flask.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
+from random import randint
+
 app = Flask(__name__)
 io = SocketIO(app)
 
@@ -23,6 +25,11 @@ def connected():
     #print("%s connected" % (request.namespace.socket.sessid))
     #clients.append(request.namespace)
 
+@io.on('requestId')
+def sendId():
+    id = randint(0,10000);
+    print("Sending id: " + str(id));
+    io.emit('id', id);
 
 @io.on('disconnect')
 def disconnect():
